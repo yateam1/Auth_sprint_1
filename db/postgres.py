3ac82774090137012f671b310e899c39a97f6238
mrukhlov <maxim.mrukhlov@3dsmile.ru>
2021-08-15 11:18:26 +0300

    .env file added

--- db/postgres.py
+++ db/postgres.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from decouple import config
 
+SQLALCHEMY_DATABASE_URI = f"""postgresql://
+    {config('POSTGRES_USERNAME')}:
+    {config('POSTGRES_PASSWORD')}@
+    {config('POSTGRES_HOST')}/
+    {config('POSTGRES_DB')
+}"""
 
 db = SQLAlchemy()
 
 
 def init_db(app: Flask):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://<username>:<password>@<host>/<database_name>'
+    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
     db.init_app(app)
 
--- db/redis.py
+++ db/redis.py
@@ -1,8 +1,11 @@
 import redis
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from decouple import config
 
 db = SQLAlchemy()
-redis_db = redis.Redis(host='localhost', port=6379, db=0)
-
-# ... 
+redis_db = redis.Redis(
+    host=config('REDIS_HOST'),
+    port=config('REDIS_PORT'),
+    db=config('REDIS_DB')
+)
