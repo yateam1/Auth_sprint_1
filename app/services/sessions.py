4357e0f63349c490b6d7070a2a978d01e5096bf3
s-klimov <s.klimov@bk.ru>
2021-08-22 12:21:00 +0700

    Добавлен код получения нового рефреш токена

--- app/services/sessions.py
+++ app/services/sessions.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from app import db
 from app.services.base import AbstractService
 from app.models import Session
 
@@ -7,8 +8,8 @@ from app.models import Session
 class SessionService(AbstractService):
     model = Session
 
-    def get(self, user, fingerprint: str, user_agent: str):
-        """Возвращает пользователя с юзернеймом username."""
+    def get_by_user(self, user, fingerprint: str, user_agent: str):
+        """Возвращает сессию указанного пользователя."""
         now = datetime.utcnow()
         return self.model.query.filter(
             self.model.user == user,
@@ -16,4 +17,21 @@ class SessionService(AbstractService):
             self.model.fingerprint == fingerprint,
             self.model.expired >= now
         ).first()
+
+    def get_by_refresh_token(self, refresh_token, fingerprint: str, user_agent: str):
+        """Возвращает сессию по рефреш токену."""
+        now = datetime.utcnow
+        return self.model.query.filter(
+            self.model.refresh_token == refresh_token,
+            self.model.user_agent == user_agent,
+            self.model.fingerprint == fingerprint,
+            self.model.expired >= now
+        ).first()
+
+    def delete(self, instance):
+        instance.expired = datetime.utcnow()
+        db.session.commit()
+        return instance
+
+
 
